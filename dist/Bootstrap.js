5693ec7264bbcb424de378456f5a51cbd46bff96
Daniel Bertolozi <daniel.bertolozi@sap.com>
2020-10-02 23:42:34 -0300

    Prototype

--- dist/Bootstrap.js
+++ dist/Bootstrap.js
@@ -1,4 +1,4 @@
 import { Fetcher } from "./Posts/Fetcher";
-console.log("bootstrap");
 var fetcher = new Fetcher();
-fetcher.importFrom("../../content");
+console.log("bootstrap");
+fetcher.importFrom("/content");
--- src/main/Posts/Fetcher.ts
+++ src/main/Posts/Fetcher.ts
@@ -1,10 +1,15 @@
 export class Fetcher {
-  public importFrom(path: string) {
+  public async importFrom(path: string) {
     // TODO: set base URL based on .env. 
     // For deploy should use fetch('https://api.github.com/repos/danielbertolozi/danielbertolozi.github.io/contents/content');
     // Deploy's URL should be at a .env
     // For local, I should create a server that mocks that, using content's content 
-    debugger;
-    console.log(test);
+    const request = new XMLHttpRequest();
+    const baseUrl = "http://localhost:9080";
+    path = "https://api.github.com/repos/danielbertolozi/danielbertolozi.github.io/contents/content";
+    request.open("GET", path);
+    request.onreadystatechange = () => alert(request.response);
+    request.send();
+    // TODO: Return promise
   }
 }
