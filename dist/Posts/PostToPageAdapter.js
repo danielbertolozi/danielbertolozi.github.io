3f45266ad13225264c6d3dc1979e0fda8fb3aa12
Daniel Bertolozi <daniel.bertolozi@sap.com>
2020-10-03 00:58:08 -0300

    somethign

--- dist/Posts/PostToPageAdapter.js
+++ dist/Posts/PostToPageAdapter.js
@@ -5,6 +5,7 @@ export class PostToPageAdapter {
         (_a = document.getElementById(divId)) === null || _a === void 0 ? void 0 : _a.appendChild(elementToAdd);
     }
     buildPost(post) {
+        // TODO: Markdown formatting here. Should check first character to determine element type, or look for **/__/~~
         const div = document.createElement("div");
         div.className = "post";
         const paragraph = document.createElement("p");
