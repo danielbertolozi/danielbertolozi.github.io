dfdc273c85a2f19030c3698a1b57519c846852b6
I852708 <daniel.bertolozi@sap.com>
2022-08-04 19:31:33 -0300

    WIP

--- src/components/pages/Photography/Photography.tsx
+++ src/components/pages/Photography/Photography.tsx
@@ -2,12 +2,24 @@ import Constants from "../../../Constants";
 import "./Photography.css";
 import { BackgroundWithOverlay } from "../../elements/BackgroundWithOverlay/BackgroundWithOverlay";
 import { PageHeader } from "../../elements/PageHeader/PageHeader";
+import { ImageOutline, LogoDropbox } from "react-ionicons";
 
 export default function Photography() {
     return (
         <BackgroundWithOverlay
           backgroundUrl={Constants.PHOTOS_BANNER_URL}>
           <PageHeader showBackButton={true} text="Fotografia"/>
+          <div className="photography-content">
+            <span> WIP</span>
+            <ImageOutline
+              height="2em"
+              width="2em"
+              color="dddddd" />
+            <LogoDropbox
+              height="2em"
+              width="2em"
+              color="dddddd" />
+          </div>
         </BackgroundWithOverlay>
     );
 }
