b1a100c8b323e20ff5e1995140be17b742c38bd0
I852708 <daniel.bertolozi@sap.com>
2022-08-04 13:37:26 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>danielbertolozi.github.io</h1>
+        <span>Lorem Y e etc</span>
+      </header>
+      <div>
+        <div>
+          <span>Profissional</span>
+        </div>
+        <div>
+          <span>Carros</span>
+        </div>
+        <div>
+          <span>Fotografia</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
